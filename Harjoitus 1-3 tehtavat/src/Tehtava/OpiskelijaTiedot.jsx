fef16279cff0b8f09f545c7b3ad0dd5af82b55bd
Tran Dinh <s2401130@edu.bc.fi>
2026-05-07 09:16:49 +0300

    Lisää OpiskelijaTiedot komponentti

---
+++ Harjoitus 1-3 tehtavat/src/Tehtava/OpiskelijaTiedot.jsx
@@ -0,0 +1,17 @@
+function OpiskelijaTiedot() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet",
+  };
+
+  return (
+    <div>
+      <h2>{opiskelija.nimi}</h2>
+      <p>Ikä: {opiskelija.ika}</p>
+      <p>Kurssi: {opiskelija.kurssi}</p>
+    </div>
+  );
+}
+
+export default OpiskelijaTiedot;
